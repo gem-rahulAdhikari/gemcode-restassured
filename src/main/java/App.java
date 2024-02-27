f13242125579b7e97a484bb32224ac125dc9245a
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-02-27 12:30:19 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,7 @@ import java.util.Map;
 public class App {
     static String reportName="Report_1709011503_0";
     public Map<String, String> demo() {
+        
         String baseURI = "https://betaapi.gemecosystem.com/gemEcosystemDashboard/actuator/health";
         RestAssured.baseURI = baseURI;
         Response response = RestAssured.given().get().then().extract().response();
