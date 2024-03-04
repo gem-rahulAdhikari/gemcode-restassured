4133e7309190156047ad71519bc184e3adc1db0f
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-03-04 10:15:43 +0530

    Update YAML and Java files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,7 +19,7 @@ public class App {
        
         x.put("headers", null);
        
-        x.put("response", response.asString());
+       
         x.put("status-code", String.valueOf(response.statusCode()));
         return x;
     }
