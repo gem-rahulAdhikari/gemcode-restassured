4ffcdc093d239fd3d6e0c9ae27e5ca8d2c858c9d
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-03-05 10:12:58 +0530

    Update YAML and Java files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,13 +7,12 @@ import java.util.Map;
 public class App {
     static String reportName="Report_1709011503_0";
     public Map<String, String> demo() {
-        
         String baseURI = "https://betaapi.gemecosystem.com/gemEcosystemDashboard/actuator/health";
         RestAssured.baseURI = baseURI;
         Response response = RestAssured.given().get().then().extract().response();
         response.prettyPrint();
 
-        //     return [base_url, method, params, headers, request body, response_body, status code]
+        // return [base_url, method, params, headers, request body, response_body, status code]
         Map<String, String> x = new HashMap<>();
         x.put("baseURL", baseURI);
         x.put("method", "get");
